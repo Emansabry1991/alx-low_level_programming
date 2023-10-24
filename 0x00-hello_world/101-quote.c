45bc813bfc078bbd9184433ede86def6a9e8ddd0
eman sabry <emansabry1991@gmail.com>
2023-10-24 11:49:51 -0700

    var

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -10,8 +11,6 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar , 2015-10-19\n";
-
-	write(1, quo, 59);
+	fput("and that piece of art is useful\" - Dora Korpar , 2015-10-19\n", stdout);
 	return (1);
 }
